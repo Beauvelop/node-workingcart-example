ca0403f2b5ab336a31effbdb8a6ac9d28971b368
Beau Jackson <beau.html@gmail.com>
2017-03-04 03:00:15 -0700

    added config file, sessionSecret

--- node-cart/controllers/cartCtrl.js
+++ node-cart/controllers/cartCtrl.js
@@ -1,7 +1,6 @@
 module.exports = {
 
   index: function(req, res, next) {
-    console.log("r.s.cart", req.session.cart);
     res.status(200).json(req.session.cart);
   },
   create: function(req, res, next) {
--- node-cart/index.js
+++ node-cart/index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const session = require('express-session');
+
+let config = require('./config.json')
+
 const phoneCtrl = require('./controllers/phoneCtrl')
 const cartCtrl = require('./controllers/cartCtrl')
 const app = express();
 
 app.use(bodyParser.json());
 app.use(session({
-  secret: "lkjsdlfkjsflj",
+  secret: config.sessionSecret,
   saveUninitialized: false,
   resave: false
 }))
@@ -29,8 +32,6 @@ app.delete('/api/phones/:id', phoneCtrl.destroy);
 app.get('/api/cart', cartCtrl.index)
 app.post('/api/cart', cartCtrl.create)
 
-
-const PORT = 4000;
-app.listen(PORT, function() {
-  console.log(`Listening on ${PORT}`)
+app.listen(config.port, function() {
+  console.log("Listening on PORT: " + config.port)
 });
